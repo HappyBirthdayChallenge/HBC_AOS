9c79cdeb44a5a17518a0d02aa2c57b350b4ff168
choi <csw3055@gmail.com>
2022-10-14 19:01:03 +0900

    [FEAT] mainactivity viewbinding

--- app/src/main/java/com/inha/hbc/ui/main/MainActivity.kt
+++ app/src/main/java/com/inha/hbc/ui/main/MainActivity.kt
@@ -3,9 +3,12 @@ package com.inha.hbc.ui.main
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.inha.hbc.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
