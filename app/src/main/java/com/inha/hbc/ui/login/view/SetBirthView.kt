ddd4d2c0fac243d6bddd9b38d831c3d5ac54be38
choi <csw3055@gmail.com>
2022-10-31 10:27:22 +0900

    [FEAT]카카오 로그인, 회원가입 로직 추가

--- app/src/main/java/com/inha/hbc/ui/login/view/SetBirthView.kt
+++
@@ -1,8 +0,0 @@
-package com.inha.hbc.ui.login.view
-
-import com.inha.hbc.data.remote.resp.NormSignin
-
-interface SetBirthView {
-    fun onSetBirthSuccess(data: NormSignin)
-    fun onSetBirthFailure(code: Int)
-}
