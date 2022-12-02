c150a008aa6e48ccc081c74e9cb02046d1443c3e
choi <csw3055@gmail.com>
2022-12-02 18:12:51 +0900

    [FIX] 친구페이지 tab과 viewpager 연결

--- app/src/main/java/com/inha/hbc/ui/menu/ui/FriendListFragment.kt
+++ app/src/main/java/com/inha/hbc/ui/menu/ui/FriendListFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.inha.hbc.databinding.FragmentMenuFriendlistBinding
 import com.inha.hbc.ui.adapter.MenuFriendPageVPAdapter
 
@@ -38,8 +40,11 @@ class FriendListFragment(val firstPos: Int): Fragment() {
         val adapter = MenuFriendPageVPAdapter(firstPos)
         binding.vpMenuFriendlist.adapter = adapter
 
-
         binding.vpMenuFriendlist.currentItem = firstPos
+
+        val titles = listOf("팔로잉", "팔로워")
+        TabLayoutMediator(binding.tlMenuFriendlist, binding.vpMenuFriendlist
+        ) { tab, position -> tab.text = titles[position] }.attach()
 
     }
 
--- app/src/main/java/com/inha/hbc/ui/menu/ui/MypageFragment.kt
+++ app/src/main/java/com/inha/hbc/ui/menu/ui/MypageFragment.kt
@@ -1,16 +1,20 @@
 package com.inha.hbc.ui.menu.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.inha.hbc.databinding.FragmentMypageBinding
 import com.inha.hbc.ui.adapter.MypageRVAdapter
+import com.inha.hbc.util.fragmentmanager.MenuFragmentManager
 
 class MypageFragment: Fragment() {
     lateinit var binding: FragmentMypageBinding
     lateinit var adapter: MypageRVAdapter
+    lateinit var backPressedCallback: OnBackPressedCallback
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,10 +27,32 @@ class MypageFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRv()
+        initListener()
     }
 
     fun initRv() {
         adapter = MypageRVAdapter()
         binding.rvMy.adapter = adapter
+    }
+
+    fun initListener() {
+        binding.ivMyBack.setOnClickListener {
+            MenuFragmentManager.close()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        backPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                MenuFragmentManager.close()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, backPressedCallback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        backPressedCallback.remove()
     }
 }
