8f076e26ff7a326b04cce55adc6ecbb85ba30c08
choi <csw3055@gmail.com>
2022-12-03 18:01:06 +0900

    [FEAT]알림함 페이지 어댑터 연결

--- app/src/main/java/com/inha/hbc/ui/main/ui/NotifyFragment.kt
+++ app/src/main/java/com/inha/hbc/ui/main/ui/NotifyFragment.kt
@@ -1,6 +1,32 @@
 package com.inha.hbc.ui.main.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.inha.hbc.databinding.FragmentNotifyBinding
+import com.inha.hbc.ui.adapter.MainNotifyRVAdapter
 
 class NotifyFragment: Fragment() {
+    lateinit var binding: FragmentNotifyBinding
+    lateinit var adapter: MainNotifyRVAdapter
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=  FragmentNotifyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRv()
+    }
+
+    fun initRv() {
+        adapter = MainNotifyRVAdapter()
+        binding.rvNotify.adapter = adapter
+    }
 }
