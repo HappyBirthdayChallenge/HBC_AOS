a6a86ddd3ba2e0bb8831d7da2037fe07b1f55982
choi <csw3055@gmail.com>
2022-12-05 21:05:42 +0900

    [FIX]메인페이지 검색 리사이클러뷰 조정

--- app/src/main/java/com/inha/hbc/ui/adapter/MainSearchRVAdapter.kt
+++ app/src/main/java/com/inha/hbc/ui/adapter/MainSearchRVAdapter.kt
@@ -1,9 +1,13 @@
 package com.inha.hbc.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.inha.hbc.R
 import com.inha.hbc.databinding.ItemUserListBinding
 import com.inha.hbc.data.remote.resp.main.Result
+import com.inha.hbc.util.fragmentmanager.MainFragmentManager
 
 class MainSearchRVAdapter(var dataArr: List<Result>): RecyclerView.Adapter<MainSearchRVAdapter.UserHolder>() {
     interface SetMainSearch{
@@ -11,19 +15,50 @@ class MainSearchRVAdapter(var dataArr: List<Result>): RecyclerView.Adapter<MainS
     }
     lateinit var setMainSearch: SetMainSearch
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder {
-        TODO("Not yet implemented")
+        val binding = ItemUserListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserHolder(binding, setMainSearch, dataArr)
     }
 
     override fun onBindViewHolder(holder: UserHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.init(position)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return dataArr.size
     }
 
-    class UserHolder(val binding: ItemUserListBinding): RecyclerView.ViewHolder(binding.root) {
+    class UserHolder(val binding: ItemUserListBinding, val setMainSearch: SetMainSearch, var dataArr: List<Result>): RecyclerView.ViewHolder(binding.root) {
+        fun init(pos: Int) {
+            initView(pos)
+        }
 
+        fun initView(pos: Int) {
+            binding.tvItemUserListId.text = dataArr[pos].member.username
+            binding.tvItemUserListName.text = dataArr[pos].member.name
+
+            if (dataArr[pos]!!.follow) {
+                binding.tvItemUserListFollow.text = "팔로잉"
+                binding.tvItemUserListFollow.setTextColor(
+                    MainFragmentManager.baseActivity.applicationContext.resources.getColor(R.color.black, null)
+                )
+                binding.tvItemUserListFollow.textSize = 14f
+                binding.tvItemUserListFollow.background =
+                    MainFragmentManager.baseActivity.applicationContext.resources.getDrawable(
+                        R.drawable.item_white_following_btn, null
+                    )
+            }
+            else {
+                binding.tvItemUserListFollow.text = "팔로우"
+                binding.tvItemUserListFollow.background = MainFragmentManager.baseActivity.applicationContext.resources.getDrawable(
+                    R.drawable.item_blue_follow_btn, null
+                )
+            }
+
+            Glide.with(MainFragmentManager.baseActivity.applicationContext)
+                .load(dataArr[pos]!!.member.image_url)
+                .into(binding.ivItemUserList)
+
+        }
     }
 
 }
--- app/src/main/java/com/inha/hbc/ui/menu/ui/NotifyFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.inha.hbc.ui.menu.ui
-
-import androidx.fragment.app.Fragment
-
-class NotifyFragment: Fragment() {
-}
