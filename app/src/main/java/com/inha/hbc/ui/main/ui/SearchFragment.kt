136b6dbdd78fbb18a7a0341491dd959604427c84
choi <csw3055@gmail.com>
2022-12-05 18:25:27 +0900

    [FEAT]검색페이지 UI 로딩화면 추가

--- app/src/main/java/com/inha/hbc/ui/main/ui/SearchFragment.kt
+++ app/src/main/java/com/inha/hbc/ui/main/ui/SearchFragment.kt
@@ -7,12 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.inha.hbc.data.remote.resp.main.GlobalSearchSuccess
 import com.inha.hbc.databinding.FragmentSearchBinding
+import com.inha.hbc.ui.main.view.GlobalSearchView
+import com.inha.hbc.util.network.main.MainRetrofitService
 import kotlin.concurrent.timer
 
-class SearchFragment: Fragment() {
+class SearchFragment: Fragment(), GlobalSearchView {
     lateinit var binding: FragmentSearchBinding
     var isTyping = false
+    var time = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -50,10 +54,25 @@ class SearchFragment: Fragment() {
     }
 
     fun startTimer() {
-        timer(period = 100) {
-            if (!isTyping) {
-
+        timer(period = 1) {
+            time++
+            if (!isTyping && time == 100) {
+                MainRetrofitService().globalSearch(binding.tieSearch.text.toString(), this@SearchFragment)
+                time = 0
+                this.cancel()
+            }
+            if (isTyping || time == 100) {
+                time = 0
+                this.cancel()
             }
         }
+    }
+
+    override fun onGlobalSearchSuccess(resp: GlobalSearchSuccess) {
+        resp.
+    }
+
+    override fun onGlobalSearchFailure() {
+        TODO("Not yet implemented")
     }
 }
