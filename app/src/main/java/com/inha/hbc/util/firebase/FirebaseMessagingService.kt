57f6a9180187c51a82856b538b06285d168aa4b2
choi <csw3055@gmail.com>
2022-12-06 06:57:39 +0900

    [FIX]포그라운드 알림 적용

--- app/src/main/java/com/inha/hbc/util/firebase/FirebaseMessagingService.kt
+++ app/src/main/java/com/inha/hbc/util/firebase/FirebaseMessagingService.kt
@@ -1,16 +1,27 @@
 package com.inha.hbc.util.firebase
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.remoteMessage
+import com.inha.hbc.R
 import com.inha.hbc.util.network.RetrofitService
 import com.inha.hbc.util.sharedpreference.GlobalApplication
 
 class FirebaseMessagingService: FirebaseMessagingService() {
-
+    val CHANNEL_ID = "default_noti"
+    val CHANNEL_NAME= "HBC_notify"
+    val CHANNEL_DESCRIPTION = "알림"
     //현재 토큰 검색(수동 실행 -> 항상 최신 토큰 받아옴)
    fun searchToken() {
         Log.d("getToken","getTok")
@@ -34,8 +45,39 @@ class FirebaseMessagingService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        if (message.notification != null) {
+        val title = message.notification?.title
+        val body = message.notification?.body
+
+        makeChannel()
+
+        buildNoti(title!!, body!!)
+
+    }
+
+    fun makeChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+
         }
+    }
+
+    fun buildNoti(title: String, message: String) {
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(title)
+            .setContentText(message)
+
+
+        NotificationManagerCompat.from(this)
+            .notify(1, notificationBuilder.build())
     }
     
 
